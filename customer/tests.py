729e5dda508e2cf5cf210bc646c822069196d1ac
Muhammad Abu Hussain Rifad <hussain.rifad89@gmail.com>
2024-08-08 09:36:25 +0300

    sql issure

--- customer/tests.py
+++ customer/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 import sqlite3
+from sqlite3 import dbapi2 as DataBase
 
+print(DataBase.sqlite_version_info)
 print(sqlite3.sqlite_version)
 # Create your tests here.

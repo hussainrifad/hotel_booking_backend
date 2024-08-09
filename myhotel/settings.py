98849a074bca547fa10982dabb120a968f3cc221
Muhammad Abu Hussain Rifad <hussain.rifad89@gmail.com>
2024-08-09 15:53:50 +0300

    test

--- myhotel/settings.py
+++ myhotel/settings.py
@@ -146,7 +146,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_USE_TLS = True
